f7367c1a02823326b428c8ef00bb1a75ad4ef1f0
Jim Merritt <jgmerritt@gmail.com>
2017-01-05 16:59:07 -0500

    First attempt to strip the prefixes from dir_ob and obj.

---
+++ connection.py
@@ -0,0 +1,80 @@
+# Imports
+import logging
+from os import walk
+from os.path import join
+from swiftclient.multithreading import OutputManager
+from swiftclient.service import SwiftService, SwiftError, SwiftUploadObject
+
+logging.basicConfig(level=logging.ERROR)
+logging.getLogger("requests").setLevel(logging.CRITICAL)
+logging.getLogger("swiftclient").setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+
+# Account information
+authVersion = '1.0'
+authURL = 'http://192.168.244.10/auth/v1.0'
+user = 'test:tester'
+key = 'testing'
+uu_threads = 20
+authDict = {"auth_version": authVersion, "auth": authURL, "user": user, "key": key, "object_uu_threads": uu_threads}
+
+directoryVar = '/home/jimm/uploadtest'
+containerVar = 'container1'
+
+with SwiftService(authDict) as swift, OutputManager() as out_manager:
+    try:
+        # Collect all the files and folders in the given directory
+        objsVar = []
+        dir_markers = []
+        for (_dir, _ds, _fs) in walk(directoryVar):
+            # print "I am here: ", _dir
+            if not (_ds + _fs):
+                dir_markers.append(_dir)
+            else:
+                objsVar.extend([join(_dir, _f) for _f in _fs])
+
+        # Now that we've collected all the required files and dir markers
+        # build the ``SwiftUploadObject``s for the call to upload
+        objsVar = [
+            SwiftUploadObject(
+                o, object_name=o.replace(
+                    directoryVar, 'objects', 1              # Modify this to remove prefix
+                )
+            ) for o in objsVar
+            ]
+        dir_markers = [
+            SwiftUploadObject(
+                None, object_name=d.replace(
+                    directoryVar, 'objects', 1              # Modify this to remove prefix
+                ), options={'dir_marker': True}
+            ) for d in dir_markers
+        ]
+
+        # Schedule uploads on the SwiftService thread pool and iterate
+        # over the results
+        for r in swift.upload(containerVar, objsVar + dir_markers):
+            if r['success']:
+                if 'object' in r:
+                    print(r['object'])
+                elif 'for_object' in r:
+                    print(
+                        '%s segment %s' % (r['for_object'],
+                                           r['segment_index'])
+                        )
+            else:
+                error = r['error']
+                if r['action'] == "create_container":
+                    logger.warning(
+                        'Warning: failed to create container '
+                        "'%s'%s", containerVar, error
+                    )
+                elif r['action'] == "upload_object":
+                    logger.error(
+                        "Failed to upload object %s to container %s: %s" %
+                        (containerVar, r['object'], error)
+                    )
+                else:
+                    logger.error("%s" % error)
+
+    except SwiftError as e:
+        logger.error(e.value)
